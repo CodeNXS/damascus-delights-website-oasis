76ec3e7a4531754a80fb551ce9ec0764906dd0f2
CodeNXS <codenexus.official1@gmail.com>
2025-06-26 13:00:23 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/damascus-delights/",
+  base: "/damascus-delights-website-oasis/",
   server: {
     host: "::",
     port: 8080,
