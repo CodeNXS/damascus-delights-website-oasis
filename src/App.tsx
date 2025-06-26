7d7bad21b64a2ac764451f142cd6b7a7c7898375
CodeNXS <codenexus.official1@gmail.com>
2025-06-26 13:03:01 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Index />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
